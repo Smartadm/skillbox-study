11166d48d2e921dca7f939fee7fae0157a8facab
smartadm <dik_off@mail.ru>
2020-04-27 01:24:49 +0400

    finish with zoo

--- lesson_002/05_zoo.py
+++ lesson_002/05_zoo.py
@@ -17,12 +17,22 @@ birds = ['rooster', 'ostrich', 'lark', ]
 #  и выведите список на консоль
 # TODO здесь ваш код
 
+zoo.extend(birds)
+print(zoo)
+
 # уберите слона
 #  и выведите список на консоль
 # TODO здесь ваш код
+
+zoo.remove('elephant')
+print(zoo)
 
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
 # TODO здесь ваш код
 
+lions_cell = zoo.index('lion') + 1
+larks_cell = zoo.index('lark') + 1
+print('Lions cell number is', lions_cell)
+print('Larks cell number is', larks_cell)
 
