c1713c9ff77f59f4411602cc69f9953cef4378b5
smartadm <dik_off@mail.ru>
2020-05-05 01:48:21 +0300

    a lot of people live in my district

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -7,6 +7,26 @@
 
 # TODO здесь ваш код
 
+from district.central_street.house1 import room1 as dcsh1r1
+from district.central_street.house1 import room2 as dcsh1r2
+from district.central_street.house2 import room1 as dcsh2r1
+from district.central_street.house2 import room2 as dcsh2r2
+from district.soviet_street.house1 import room1 as dssh1r1
+from district.soviet_street.house1 import room2 as dssh1r2
+from district.soviet_street.house2 import room1 as dssh2r1
+from district.soviet_street.house2 import room2 as dssh2r2
 
+dist_folks = []
+dist_folks.extend(dcsh1r1.folks)
+dist_folks.extend(dcsh1r2.folks)
+dist_folks.extend(dcsh2r1.folks)
+dist_folks.extend(dcsh2r2.folks)
+dist_folks.extend(dssh1r1.folks)
+dist_folks.extend(dssh1r2.folks)
+dist_folks.extend(dssh2r1.folks)
+dist_folks.extend(dssh2r2.folks)
+
+all_folks = ', '.join(dist_folks)
+print('In my district live', all_folks)
 
 
