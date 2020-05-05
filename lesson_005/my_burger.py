5a7cb4d7a1f8f0254a949f10d4e19007838b414f
smartadm <dik_off@mail.ru>
2020-05-05 16:25:29 +0300

    create my_burger module

---
+++ lesson_005/my_burger.py
@@ -0,0 +1,21 @@
+def add_bread(count):
+    print('Add', count, 'bread')
+
+def add_cucumber(count):
+    print('Add', count, ' cucumber')
+
+def add_tomato(count):
+    print('Add', count, ' tomato')
+
+def add_cheese(count):
+    print('Add', count, ' cheese')
+
+def add_mayonase(count):
+    print('Add', count, ' mayonase')
+
+def add_cutlet(count):
+    print('Add', count, ' cutlet')
+
+def add_onion(count):
+    print('Add', count, ' onion')
+
